91a6054253a9046377ac242f73bb9e833f5685b7
Vladislav Shilov <Shilovvlad@mail.ru>
2022-12-02 20:15:04 +0300

    add Dog, Timer bean

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,10 +1,26 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("singleton")
 public class Timer {
 
+    @Autowired
+    public Timer() {
+        System.out.println("Timer created");
+    }
+
+//    private final Timer timer;
     private Long nanoTime = System.nanoTime();
 
     public Long getTime() {
         return nanoTime;
     }
+//
+//    public Timer getTimer() {
+//        return timer;
+//    }
 }
